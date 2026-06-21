d4c34e89ab9afcd6c5352ebed40d2e29bd20b877
shama k <167509718+Shama-k17@users.noreply.github.com>
2026-06-21 15:31:14 +0530

    Time: 1257 ms (60.47%), Space: 0B (100%) - LeetHub

---
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -0,0 +1,8 @@
+SELECT 
+    U.user_id AS buyer_id, 
+    U.join_date, 
+    IFNULL(SUM(EXTRACT(YEAR FROM O.order_date) = 2019), 0) AS orders_in_2019
+FROM Users U
+LEFT JOIN Orders O 
+    ON U.user_id = O.buyer_id
+GROUP BY U.user_id, U.join_date;
